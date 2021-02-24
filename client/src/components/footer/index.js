ddc804c0b6f35bc09a44e3b88058c03620da30b0
Tim Holzer <timiholzer@gmail.com>
2021-02-23 18:28:01 -0700

    setup react components

---
+++ client/src/components/footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h3 class="nav-item">
+        <a class="nav-link" href="https://github.com/timholzer">Built by Tim Holzer</a>
+      </h3>
+      <h3 class="nav-item">
+        <a class="nav-link" href="https://github.com/timholzer/CMS-Style-Blogsite">Site Repo</a> 
+      </h3>
+    </footer>
+  );
+}
+
+export default Footer;
